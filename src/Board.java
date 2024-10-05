825e710c116250ca39bb4e0ade0d0fcdcc5e0ac5
IWantDayDayUp <1462959616@qq.com>
2024-10-05 19:24:55 +0100

    update Board

--- src/Board.java
+++ src/Board.java
@@ -5,6 +5,11 @@ public class Board {
     public LanePile[] lanePiles;
     public SuitPile[] suitPiles;
 
+    public Score score;
+    public int moveCnt;
+
+    public boolean isNewBoard;
+
     public Board() {
 
         this.deck = new Deck();
@@ -13,6 +18,11 @@ public class Board {
         this.drawPile = new DrawPile();
         this.lanePiles = new LanePile[7];
         this.suitPiles = new SuitPile[4];
+
+        this.score = new Score();
+        this.moveCnt = 0;
+
+        this.isNewBoard = true;
 
         // - generate all cards, then shuffle to get lines/suits/foundation
         // initLines();
@@ -48,12 +58,31 @@ public class Board {
         this.displaySuitPile();
         System.out.println();
         this.displayLinePile();
+
+        this.displayGameInfo();
     }
 
     public void displayDrawPile() {
         System.out.println("  P: " + this.drawPile);
     }
 
+    public void displayLinePile() {
+        int maxSize = 0;
+        for (LanePile lane : this.lanePiles) {
+            maxSize = Math.max(maxSize, lane.getSize());
+        }
+
+        System.err.println(" 01  02  03  04  05  06  07");
+        for (int i = 0; i < maxSize; i++) {
+            for (LanePile lane : this.lanePiles) {
+                if (lane.getSize() > i) {
+                    System.out.print(lane.cards.get(lane.getSize() - 1 - i));
+                } else {
+                    System.out.print("    ");
+                }
+            }
+        }
+    }
 
     public void displaySuitPile() {
         for (int i = 0; i < 4; i++) {
@@ -61,9 +90,53 @@ public class Board {
         }
     }
 
+    public void displayGameInfo() {
+        // TODO
+        System.err.println(this.score + " " + this.moveCnt);
+    }
 
     public boolean draw() {
         return this.drawPile.draw();
     }
+    public void displayWelcome() {
+        System.out.println("Hi!");
+    }
 
+    public void displayCommandNotExecuted() {
+        // TODO
+        System.out.println("Hi!");
+
+    }
+
+    public void doCommand(Command command) {
+        boolean isSuccess;
+        if (command.isDraw()) {
+            isSuccess = this.drawPile.draw();
+        } else {
+            isSuccess = this.moveCard(command);
+        }
+
+        this.updateBoard(isSuccess, command);
+    }
+
+    public boolean isGameOver() {
+        // TODO
+        for (SuitPile suit : this.suitPiles) {
+            if (suit.getSize() != 13) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void updateBoard(boolean isSuccess, Command command) {
+        if (isSuccess) {
+            this.isNewBoard = true;
+            this.score.update(command);
+            this.moveCnt += 1;
+        } else {
+            this.isNewBoard = false;
+        }
+    }
 }
