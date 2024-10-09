a258196bb468496440ec6aba15a77c2db1b69cf1
IWantDayDayUp <1462959616@qq.com>
2024-10-09 23:06:35 +0100

    Add Main

---
+++ src/Patience.java
@@ -0,0 +1,30 @@
+/**
+ * <p> Project Name: Patience Game </p>
+ * <P> Course Name: COMP41670 Software Engineering </P>
+ * <P> Description: Patience (Europe), card solitaire or solitaire (US/Canada), is a genre of card games whose common feature is that the aim is to arrange the cards in some systematic order or, in a few cases, to pair them off in order to discard them. </P>
+ * <P> Shaofeng Wang </P>
+ * <P> 24202293 </P>
+ */
+public class Patience {
+
+    public static void main(String[] args) {
+        Board board = new Board();
+
+        board.displayFirstSatrt();
+
+        do {
+            board.displayBoard();
+            Command command = new Command();
+            command.getCommand();
+            if (command.isQuit()) {
+                break;
+            }
+
+            if (board.isValidMove(command)) {
+                board.executeCommand(command);
+            } else {
+                board.displayCommandNotExecuted();
+            }
+        } while (!board.isGameOver());
+    }
+}
