07f7c95aac36895e29d454ad1aa5a4a80bd18b45
IWantDayDayUp <1462959616@qq.com>
2024-10-09 10:30:38 +0100

    Update class 'LanePile'

--- src/LanePile.java
+++ src/LanePile.java
@@ -1,18 +1,23 @@
 import java.util.ArrayList;
 
+/**
+ * A model for the LanePile, inherits from class 'Pile'.
+ * 
+ * Add additional implementations as follows:
+ * - Add/Remove multiple cards
+ * - Verify that the new card can add to the LanePile based on the value and
+ * color of the new card
+ */
 public class LanePile extends Pile {
-    public final String lineLabel;
 
-    public LanePile(String lineLabel) {
-        this.lineLabel = lineLabel;
-    }
+    public LanePile() {
 
-    public String getLineLabel() {
-        return this.lineLabel;
     }
 
     /**
      * Add a few of cards to the pile by calling 'this.addCard()'
+     * 
+     * @param cards the cards to be removed
      */
     public void addCards(ArrayList<Card> cards) {
         for (Card card : cards) {
@@ -21,15 +26,48 @@ public class LanePile extends Pile {
     }
 
     /**
-     * Remove a few of cards(the top ones) from the pile by calling
-     * 'this.removeCard()'
+     * Remove a few of cards from the pile
      * 
-     * @param num: the number of cards to be removed
+     * @param num the number of cards to be removed
      */
     public void removeCards(int num) {
-
         for (int i = 0; i < num; i++) {
-            // TODO
+            this.removeCard();
         }
+    }
+
+    /**
+     * Verify that the card can add to this LanePile based on follows:
+     * 
+     * - the colors should be different
+     * - the values should be sequential and descending
+     */
+    @Override
+    public boolean isNextCard(Card card) {
+        return this.isNextValue(card) && !this.isSameColor(card);
+    }
+
+    /**
+     * Verify that card values are sequential and descending (less by 1)
+     */
+    @Override
+    public boolean isNextValue(Card card) {
+        if (this.getSize() == 0) {
+            return true;
+        }
+
+        return this.cards.getLast().getCardNumber() - card.getCardNumber() == 1;
+    }
+
+    /**
+     * Verify that whether the new card is the same color as the last card
+     */
+    @Override
+    public boolean isSameColor(Card card) {
+        if (this.getSize() == 0) {
+            return true;
+        }
+
+        return this.cards.getLast().getColor().equals(card.getColor());
     }
 }
