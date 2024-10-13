2b6ee640f0c4bb82e274d8be582e5698a7477aa8
IWantDayDayUp <1462959616@qq.com>
2024-10-13 14:20:35 +0100

    add some comments

--- src/Score.java
+++ src/Score.java
@@ -11,16 +11,18 @@ public class Score {
      */
     public void update(Command command) {
 
-        if (!command.isMove())
-        {
+        if (!command.isMove()) {
             return;
         }
 
-        if (command.isMoveFromDraw()) {
+        if (command.isMoveFromDraw() && command.isMoveToSuit()) {
+            // 10 points per card moving from the uncovered pile to one of the suits
             this.score += 10;
         } else if (command.isMoveFromLane() && command.isMoveToSuit()) {
+            // 20 points per card moving from one of the lanes to one of the suits
             this.score += 20 * command.numCards;
         } else {
+            // 5 points per card moving between lanes
             this.score += 5 * command.numCards;
         }
     }
